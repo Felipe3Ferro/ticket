5c7d546bf112bdcaa817a936adf6180e27eaa236
Felipe3Ferro <felipeferro189@gmail.com>
2026-03-15 16:50:55 -0300

    init

---
+++ src/test/java/br/ferro/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ferro.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
