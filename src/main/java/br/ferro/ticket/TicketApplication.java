a401473d3ebdde2bb00860e5d7257fb3b5277497
Felipe3Ferro <felipeferro189@gmail.com>
2026-03-22 02:25:56 -0300

    test: Add test comment to main method

--- src/main/java/br/ferro/ticket/TicketApplication.java
+++ src/main/java/br/ferro/ticket/TicketApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketApplication {
 
+  // Test commitollama
   public static void main(String[] args) {
     SpringApplication.run(TicketApplication.class, args);
     System.out.println("\uD83C\uDF89 Aplicação iniciada com sucesso! \uD83C\uDF89");
